77fbcef24219143f9ba2762910a0643cb3beee66
bocoy <bocoy51@gmail.com>
2023-10-15 17:05:52 +0900

    영속성 컨텍스트 안에서 조회시 DB에서 바로 조회하지 않고 1차캐시에서 조회함, INSERT의 경우 commit시에 실제 insert

--- src/main/java/hellojpa/jpaMain.java
+++ src/main/java/hellojpa/jpaMain.java
@@ -17,15 +17,20 @@ public class jpaMain {
         try {
             //비영속
             Member member = new Member();
-            member.setId(100L);
+            member.setId(101L);
             member.setName("HelloJPA");
+            em.persist(member);
 
             //영속
             System.out.println("====== Before ======");
-            em.persist(member);
+            em.persist(member); //이때 insert되지 않는다.
             System.out.println("====== After ======");
-            tx.commit();
 
+            Member findMember = em.find(Member.class, 101L); //DB에서 조회하지 않고 1차캐시에서 조회
+            System.out.println("findMember.id = "+ findMember.getId());
+            System.out.println("findMember.name = "+ findMember.getName());
+
+            tx.commit(); //이때 실제 insert
         } catch (Exception e) {
             tx.rollback();
         } finally {
